427018d5d4df9fc1821f0deda0cbe73196bd07ae
babyfrederic <cda.frederic.baby@gmail.com>
2021-01-05 10:29:47 +0100

    mard

--- js/clicjs.js
+++ js/clicjs.js
@@ -20,8 +20,8 @@ window.addEventListener('load',horloge);
         }
       });
 
+/*** compteur par categorie */
 
-/*** valeurs pour les entrees et sorties ***/
     var i = 0;
     var o = 0;
     var ii= 0;
@@ -31,61 +31,41 @@ window.addEventListener('load',horloge);
     var tour = 1;
     var statresult = 1 ;
     var total = 0 ;
-	function Count() {
-        document.getElementById("inputa").innerHTML = i++ + 1;
+	function Countsty() {
+        document.getElementById("inputsty").innerHTML = i++ + 1;
     }
-    function Countis() {
-        document.getElementById("inputas").innerHTML = coi++ + 1;
+    function Countill() {
+        document.getElementById("inputill").innerHTML = coi++ + 1;
     }
-	function Count_1() {
-        document.getElementById("outputa").innerHTML = o++ + 1;
+	function Countmep() {
+        document.getElementById("inputmep").innerHTML = o++ + 1;
     }
-    function Countos_1() {
-        document.getElementById("outputas").innerHTML = coo++ + 1;
+    function Countosemo() {
+        document.getElementById("inputemo").innerHTML = coo++ + 1;
 	}
-	function ecart(){
-	   var ii = document.getElementById("inputa").innerText;
-	   var ou = document.getElementById("outputa").innerText;
-	   document.getElementById("resultat").innerHTML = ii - ou;
+	function Somme(){
+	   var ii = document.getElementById("inputsty").innerText;
+     var oo = document.getElementById("inputill").innerText;
+     var uu = document.getElementById("inputmep").innerText;
+     var ee = document.getElementById("inputemo").innerText;
+	   document.getElementById("resultat").innerHTML = ee + ii + oo + uu ;
 	}
 	function zero(){
-	    document.getElementById("inputa").innerHTML = 0;
-	    document.getElementById("outputa").innerHTML =0;
+	    document.getElementById("inputsty").innerHTML = 0;
+      document.getElementById("inputill").innerHTML =0;
+      document.getElementById("inputmep").innerHTML =0;
+      document.getElementById("inputemo").innerHTML =0;
 	    document.getElementById("resultat").innerHTML =0;
 	    i = 0;
-        o = 0;
+      o = 0;
     }
-     function garde(){
-        var sta = document.getElementById("inputas").innerText;
-        var stb = document.getElementById("outputas").innerText;
-        var tot = document.getElementById("resultatsab").innerHTML =(sta - stb);
-    }
-    function peri(){
-        document.getElementById("uptour").innerHTML = tour++ + 1;
+    
+    function datechoix(){
+     var datemo = document.getElementById("dateselect").innerHTML;
     }
     
      /**** partie javascript statistiques **/
 
-    function zerostat(){
-    var i = 0;
-    var o = 0;
-    var ii= 0;
-    var ou= 0;
-    var coi = 0 ;
-    var coo =0 ;
-    var tour = 1;
-    var statresult = 1 ;
-    var total = 0 ;
-    var sta =0;
-    var stb=0;
-    var tot =0;
-        document.getElementById("inputa").innerHTML = 0;
-        document.getElementById("outputa").innerHTML =0;
-        document.getElementById("resultat").innerHTML =0;
-        document.getElementById("inputas").innerHTML = 0;
-	    document.getElementById("outputas").innerHTML =0;
-        document.getElementById("resultatsab").innerHTML =0;    
-        document.getElementById("uptour").innerHTML =1;
     }
 /*** valeurs pour le seuil d'alerte ***/
 /** calcul du seuil recommandé */
@@ -96,7 +76,6 @@ function myseuil(){
   var seuil = surface / perisecu;
   document.getElementById("leseuil").innerHTML = seuil;
 }
-
 
     
     function myFunction() {
